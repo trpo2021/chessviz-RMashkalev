72cbc61ba8efb927d14b218a481d4b2ef699254e
RMashkalev <rmashkalev@mail.ru>
2021-03-11 17:42:08 +0700

    Added pawn movement

--- chess.c
+++ chess.c
@@ -1,50 +1,75 @@
 #include <stdio.h>
 #include <ctype.h> 
 
+void display(char board[9][9])
+{
+    int i, j;
+    for (i = 0; i < 9; i++) {
+        for (j = 0; j < 9; j++) {
+            printf("%2c ", board[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main()
 {
     int i, j;
-    char array[9][9] = {};
+    char board[9][9] = {}, turn[6] = {};
 
     for (i = 0; i < 9; i++) {
         for (j = 0; j < 9; j++) {
             if ((j == 0) & (i != 8)) {
-                array[i][j] = (56 - i);
+                board[i][j] = (56 - i);
             }
             if ((i == 8) & (j != 0)) {
-                array[i][j] = (96 + j);
+                board[i][j] = (96 + j);
             }
         }
     }
 
     for (i = 1; i < 9; i++) {
-        array[1][i] = 'p';
+        board[1][i] = 'p';
     }
     
     for (i = 1; i < 9; i++) {
-        array[6][i] = 'P';
+        board[6][i] = 'P';
     }
 
-    array[0][1] = 'r';
-    array[0][2] = 'n';
-    array[0][3] = 'b';
-    array[0][4] = 'q';
-    array[0][5] = 'k';
-    array[0][6] = 'b';
-    array[0][7] = 'n';
-    array[0][8] = 'r';
-    array[8][0] = ' ';
+    board[0][1] = 'r';
+    board[0][2] = 'n';
+    board[0][3] = 'b';
+    board[0][4] = 'q';
+    board[0][5] = 'k';
+    board[0][6] = 'b';
+    board[0][7] = 'n';
+    board[0][8] = 'r';
+    board[8][0] = ' ';
 
     for (i = 1; i < 9; i++) {
-        array[7][i] = toupper(array[0][i]);
+        board[7][i] = toupper(board[0][i]);
     }
 
-    for (i = 0; i < 9; i++) {
-        for (j = 0; j < 9; j++) {
-            printf("%2c ", array[i][j]);
+    for (i = 2; i < 6; i++) {
+        for (j = 1; j < 9; j++) {
+            board[i][j] = ' ';
         }
-        printf("\n");
     }
+
+    display(board);
+
+    scanf("%s", turn);
+
+    if((board[(56 - turn[1])][(turn[0] - 96)] == 'P') && (turn[4] - turn[1] < 3)) {
+        board[(56 - turn[1])][(turn[0] - 96)] = ' ';
+        board[(56 - turn[4])][(turn[3] - 96)] = 'P';
+    }
+    if((board[(56 - turn[1])][(turn[0] - 96)] == 'p') && (turn[1] - turn[4] < 3)) {
+        board[(56 - turn[1])][(turn[0] - 96)] = ' ';
+        board[(56 - turn[4])][(turn[3] - 96)] = 'p';
+    }
+
+    display(board);
 
     return 0;
 }
