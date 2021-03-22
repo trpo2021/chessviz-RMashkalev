d49961acd2b06edc10e73b7541d93bc5624268a3
RMashkalev <rmashkalev@mail.ru>
2021-03-22 10:33:01 +0700

    Move the functions to separate files

--- src/pawn.c
+++ src/pawn.c
@@ -1,56 +1,6 @@
 #include <stdio.h>
 #include "pawn.h"
 
-void pawnmove(char board[9][9], char turn[6], int pawnp[9], int pawnP[9])
-{
-    int asciinumber = 56, asciiletter = 96;
-
-    if((board[(asciinumber - turn[1])][(turn[0] - asciiletter)] == 'P') && (turn[4] - turn[1] < 3) && (pawnP[(turn[0] - asciiletter)] == 0)) 
-    {
-        board[(asciinumber - turn[1])][(turn[0] - asciiletter)] = ' ';
-        board[(asciinumber - turn[4])][(turn[3] - asciiletter)] = 'P';
-        pawnP[(turn[0] - asciiletter)]++;
-    } else
-    if((board[(asciinumber - turn[1])][(turn[0] - asciiletter)] == 'P') && (turn[4] - turn[1] < 2) && (pawnP[(turn[0] - asciiletter)] != 0)) 
-    {
-        board[(asciinumber - turn[1])][(turn[0] - asciiletter)] = ' ';
-        board[(asciinumber - turn[4])][(turn[3] - asciiletter)] = 'P';
-    } else
-    if((board[(asciinumber - turn[1])][(turn[0] - asciiletter)] == 'p') && (turn[1] - turn[4] < 3) && (pawnp[(turn[0] - asciiletter)] == 0)) 
-    {
-        board[(asciinumber - turn[1])][(turn[0] - asciiletter)] = ' ';
-        board[(asciinumber - turn[4])][(turn[3] - asciiletter)] = 'p';
-        pawnp[(turn[0] - asciiletter)]++;
-    } else                                                                                                          
-    if((board[(asciinumber - turn[1])][(turn[0] - asciiletter)] == 'p') && (turn[1] - turn[4] < 2) && (pawnp[(turn[0] - asciiletter)] != 0)) 
-    {
-        board[(asciinumber - turn[1])][(turn[0] - asciiletter)] = ' ';
-        board[(asciinumber - turn[4])][(turn[3] - asciiletter)] = 'p';
-    }
-}
-
-void pawnattack(char board[9][9], char turn[6], int pawnp[9], int pawnP[9])
-{
-    int asciinumber = 56, asciiletter = 96;
-
-    if((board[(asciinumber - turn[4])][(turn[3] - asciiletter)] != ' ') && (board[(asciinumber - turn[1])][(turn[0] - asciiletter)] == 'P')) {
-        board[(asciinumber - turn[1])][(turn[0] - asciiletter)] = ' ';
-        board[(asciinumber - turn[4])][(turn[3] - asciiletter)] = 'P';
-    } else
-    if((board[(asciinumber - turn[4])][(turn[3] - 95)] != ' ') && (board[(asciinumber - turn[1])][(turn[0] - asciiletter)] == 'P')) {
-        board[(asciinumber - turn[1])][(turn[0] - asciiletter)] = ' ';
-        board[(asciinumber - turn[4])][(turn[3] - 95)] = 'P';
-    } else
-    if((board[(asciinumber - turn[4])][(turn[3] - asciiletter)] != ' ') && (board[(asciinumber - turn[1])][(turn[0] - asciiletter)] == 'p')) {
-        board[(asciinumber - turn[1])][(turn[0] - asciiletter)] = ' ';
-        board[(asciinumber - turn[4])][(turn[3] - asciiletter)] = 'p';
-    } else
-    if((board[(asciinumber - turn[4])][(turn[3] - 95)] != ' ') && (board[(asciinumber - turn[1])][(turn[0] - asciiletter)] == 'p')) {
-        board[(asciinumber - turn[1])][(turn[0] - asciiletter)] = ' ';
-        board[(asciinumber - turn[4])][(turn[3] - 95)] = 'p';
-    }
-}
-
 void pawn(char board[9][9],int pawnp[9], int pawnP[9])
 {
     int asciinumber = 56, asciiletter = 96;
--- src/pawn.h
+++ src/pawn.h
@@ -3,4 +3,8 @@
 
 void pawn(char board[9][9],int pawnp[9], int pawnP[9]);
 
+void pawnmove(char board[9][9], char turn[6], int pawnp[9], int pawnP[9]);
+
+void pawnattack(char board[9][9], char turn[6], int pawnp[9], int pawnP[9]);
+
 #endif
