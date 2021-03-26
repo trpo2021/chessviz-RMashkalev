29cca83607b5ee5d2411def6069ce29d6c68ffed
RMashkalev <rmashkalev@mail.ru>
2021-03-26 12:02:49 +0700

    Changed the project structure

---
+++ obj/src/pawnattack.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "pawn.h"
+
+void pawnattack(char board[9][9], char turn[6], int pawnp[9], int pawnP[9])
+{
+    int asciinumber = 56, asciiletter = 96;
+
+    if((board[(asciinumber - turn[4])][(turn[3] - asciiletter)] != ' ') && (board[(asciinumber - turn[1])][(turn[0] - asciiletter)] == 'P')) {
+        board[(asciinumber - turn[1])][(turn[0] - asciiletter)] = ' ';
+        board[(asciinumber - turn[4])][(turn[3] - asciiletter)] = 'P';
+    } else
+    if((board[(asciinumber - turn[4])][(turn[3] - 95)] != ' ') && (board[(asciinumber - turn[1])][(turn[0] - asciiletter)] == 'P')) {
+        board[(asciinumber - turn[1])][(turn[0] - asciiletter)] = ' ';
+        board[(asciinumber - turn[4])][(turn[3] - 95)] = 'P';
+    } else
+    if((board[(asciinumber - turn[4])][(turn[3] - asciiletter)] != ' ') && (board[(asciinumber - turn[1])][(turn[0] - asciiletter)] == 'p')) {
+        board[(asciinumber - turn[1])][(turn[0] - asciiletter)] = ' ';
+        board[(asciinumber - turn[4])][(turn[3] - asciiletter)] = 'p';
+    } else
+    if((board[(asciinumber - turn[4])][(turn[3] - 95)] != ' ') && (board[(asciinumber - turn[1])][(turn[0] - asciiletter)] == 'p')) {
+        board[(asciinumber - turn[1])][(turn[0] - asciiletter)] = ' ';
+        board[(asciinumber - turn[4])][(turn[3] - 95)] = 'p';
+    }
+}
